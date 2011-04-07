086605108e56a95fc79bc37bfef042589044057f
niraj <nirajlulay@csu.fullerton.edy>
2011-04-07 16:57:46 -0700

    new app

---
+++ config/initializers/pusher.rb
@@ -0,0 +1,3 @@
+Pusher.app_id = '4927'
+Pusher.key = 'ae6ec11529b0cd9b5189'	
+Pusher.secret = '3c5643a452ac246db631'
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chat::Application.config.secret_token = '2d755a286a69ea6c9105a08cc8829844a791c19a561b7f7219afe490c2297fae21cae04e2195dcbeeefce78a4d44f31194068ad3976152decd52d1c7f855497c'
